1dbb195aef3b6d9498a1fe32a6edc1eba8f3976c
Gustavo Vinicius <gus.tec.dev@gmail.com>
2025-03-16 12:44:32 -0300

    [adjust - logic - complet] (organização inicial do projeto) Criando diretorios e paginas inicias do projeto

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page() {
+    return (
+        <div className="flex justify-center">
+            <h1>register</h1>
+        </div>
+    );
+}
---
+++ src/services/sheetService.ts
@@ -0,0 +1,1 @@
+// rotas relacionadas as fichas
